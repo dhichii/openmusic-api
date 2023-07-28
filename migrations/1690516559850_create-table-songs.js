8e81ba12b1ac71f09e85c9a14006bcd5b40ae398
Adhicitta Masran <adhicittamasran@gmail.com>
2023-07-28 11:58:40 +0800

    add pg migrator

---
+++ migrations/1690516559850_create-table-songs.js
@@ -0,0 +1,40 @@
+/* eslint-disable camelcase */
+
+exports.shorthands = undefined;
+
+exports.up = (pgm) => {
+  pgm.createTable('songs', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    title: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    year: {
+      type: 'SMALLINT',
+      notNull: true,
+    },
+    genre: {
+      type: 'VARCHAR(255)',
+      notNull: true,
+    },
+    performer: {
+      type: 'TEXT',
+      notNull: true,
+    },
+    duration: {
+      type: 'SMALLINT',
+    },
+    albumId: {
+      type: 'VARCHAR(50)',
+      references: 'albums(id)',
+      onDelete: 'SET NULL',
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('songs');
+};
