da1cdde65623e58fad4e1bf078dfa158b0920012
Adhicitta Masran <adhicittamasran@gmail.com>
2023-07-29 14:26:40 +0800

    add users migrator

---
+++ migrations/1690611741386_create-table-users.js
@@ -0,0 +1,24 @@
+exports.up = (pgm) => {
+  pgm.createTable('users', {
+    id: {
+      type: 'VARCHAR(50)',
+      primaryKey: true,
+    },
+    username: {
+      type: 'VARCHAR(255)',
+      notNull: true,
+    },
+    password: {
+      type: 'VARCHAR(255)',
+      notNull: true,
+    },
+    fullname: {
+      type: 'VARCHAR(255)',
+      notNull: true,
+    },
+  });
+};
+
+exports.down = (pgm) => {
+  pgm.dropTable('users');
+};
