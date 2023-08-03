196f277b2d20c55326ba306bae8f78ee9b595cfa
Adhicitta Masran <adhicittamasran@gmail.com>
2023-08-03 15:24:58 +0800

    init s3 storage service

--- src/api/uploads/handler.js
+++ src/api/uploads/handler.js
@@ -1,5 +1,4 @@
 const autoBind = require('auto-bind');
-const config = require('../../util/config');
 
 class UploadAlbumsHandler {
   constructor(albumsService, storageService, validator) {
@@ -16,14 +15,13 @@ class UploadAlbumsHandler {
     this._validator.validateAlbumHeaders(cover.hapi.headers);
 
     const album = await this._albumsService.getAlbumById(id);
-    const filename = await this._storageService.writeFile(cover, cover.hapi);
+    const fileUrl = await this._storageService.writeFile(cover, cover.hapi);
     if (album.coverUrl) {
       const coverFilename = album.coverUrl.split('/').at(-1);
       await this._storageService.deleteFile(coverFilename);
     }
 
-    const coverUrl = `http://${config.app.host}:${config.app.port}/upload/images/${filename}`;
-    await this._albumsService.editCoverAlbumById(id, coverUrl);
+    await this._albumsService.editCoverAlbumById(id, fileUrl);
 
     return h.response({
       status: 'success',
